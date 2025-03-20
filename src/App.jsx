6d35e846bda876b40303b28a628b2bc8c1e57a82
nguyen1976 <nguyen2202794@gmail.com>
2025-03-20 16:19:10 +0700

    add Sentry integration and new components for enhanced functionality and Done

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,21 @@ import Hero from './componets/Hero'
 import Highlights from './componets/Highlights'
 import Navbar from './componets/Navbar'
 import Model from './componets/Model'
+import Features from './componets/Features'
+import HowItWorks from './componets/HowItWorks'
+import Footer from './componets/Footer'
 
 const App = () => {
+
   return (
     <main className='bg-black'>
       <Navbar />
       <Hero />
       <Highlights />
       <Model />
+      <Features />
+      <HowItWorks />
+      <Footer />
     </main>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import * as Sentry from '@sentry/react'
+
+Sentry.init({
+  dsn: 'https://1101fd474b7a7054f1499bd166f0b860@o4509008831840256.ingest.de.sentry.io/4509008853794896',
+})
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
