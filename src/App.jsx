fa1196cac9c00d75b5b6169cf1022f34651bd117
nguyen1976 <nguyen2202794@gmail.com>
2025-03-20 13:51:59 +0700

    add initial project structure with assets, configuration, and components

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Hero from './componets/Hero'
+import Highlights from './componets/Highlights'
+import Navbar from './componets/Navbar'
+import Model from './componets/Model'
+
+const App = () => {
+  return (
+    <main className='bg-black'>
+      <Navbar />
+      <Hero />
+      <Highlights />
+      <Model />
+    </main>
+  )
+}
+
+export default App
